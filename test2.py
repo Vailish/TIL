897d9b2f43402a614cec2d45390b289f1b6cb340
vailish <joyksj94@gmail.com>
2022-07-28 17:52:30 +0900

    upload

--- test2.py
+++
@@ -1,7 +0,0 @@
-print('⅛'.isnumeric()) # True
-print('⅛'.isdigit()) # False
-print('⅛'.isdecimal()) # False
-
-print('⑦'.isnumeric()) # True
-print('⑦'.isdigit()) # True
-print('⑦'.isdecimal()) # False
