a438a77c5ebeac565f5e5e133d547d6644359e31
Vailish <joyksj94@gmail.com>
2022-09-08 15:52:41 +0900

    update

--- Django/01_django/pages/urls.py
+++ Django/01_django/pages/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 app_name = 'pages'
 urlpatterns = [
-<<<<<<< HEAD
     path('index/', views.index, name='index'),
-=======
-    path('index/', views.index, name='index')
->>>>>>> 9ebe32d746a1848c36cd848281508e3f726f8c06
+    path('index/', views.index, name='index'),
 ]
