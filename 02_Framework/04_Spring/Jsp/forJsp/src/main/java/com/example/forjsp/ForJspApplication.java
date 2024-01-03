cf603523c494991f4c4a8f18a990369471508be4
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2024-01-04 07:16:16 +0900

    update

---
+++ 02_Framework/04_Spring/Jsp/forJsp/src/main/java/com/example/forjsp/ForJspApplication.java
@@ -0,0 +1,13 @@
+package com.example.forjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForJspApplication.class, args);
+	}
+
+}
---
+++ 02_Framework/04_Spring/Jsp/forJsp/src/test/java/com/example/forjsp/ForJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
